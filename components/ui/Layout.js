74eb5d6e3c9321e6450562c18c4b0d8715e8df02
Ariel Biton <ariel@zplinux.com>
2018-02-27 14:01:34 +0100

    Mobile layout added and fixed

--- components/ui/Layout.js
+++ components/ui/Layout.js
@@ -4,6 +4,7 @@ const Layout = (props) => (
   <div style={layoutStyle}>
     <Head>
       <link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css' />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     </Head>
     <main>
         {props.children}
--- hoc/ui/Responsive.js
+++ hoc/ui/Responsive.js
@@ -67,21 +67,17 @@ class MobileContainer extends Component {
         return (
             <Responsive {...Responsive.onlyMobile}>
                 <Sidebar.Pushable>
-                    <Sidebar as={Menu} animation='uncover' inverted vertical visible={sidebarOpened}>
-                        <Menu.Item as='a' active>Home</Menu.Item>
-                        <Menu.Item as='a'>Work</Menu.Item>
-                        <Menu.Item as='a'>Company</Menu.Item>
-                        <Menu.Item as='a'>Careers</Menu.Item>
-                        <Menu.Item as='a'>Log in</Menu.Item>
-                        <Menu.Item as='a' primary>Sign Up</Menu.Item>
+                    <Sidebar as={Menu} animation='uncover' inverted vertical visible={sidebarOpened} width='thin'>
+                        <Link href='/'><Menu.Item as='a'>Home</Menu.Item></Link>
+                        <Link href='/about'><Menu.Item as='a'>About</Menu.Item></Link>
                     </Sidebar>
 
-                    <Sidebar.Pusher dimmed={sidebarOpened} onClick={this.handleToggle} style={{ minHeight: '100vh' }}>
-                        <Segment inverted textAlign='center' style={{ minHeight: 350, padding: '1em 0em' }} vertical>
-                            <Container>
+                    <Sidebar.Pusher dimmed={sidebarOpened} style={{ minHeight: '100vh' }}>
+                        <Segment inverted textAlign='center' style={{ minHeight: 100,  background: '#fff', padding: '1em 0em' }} vertical>
+                            <Container style={{background: '#0f5866'}}>
                                 <Menu inverted pointing secondary size='large'>
                                     <Menu.Item onClick={this.handleToggle}>
-                                        <Icon name='sidebar' />
+                                        <Icon name='sidebar'/>
                                     </Menu.Item>
                                     <Menu.Item position='right'>
                                         <Button as='a' inverted>Log in</Button>
