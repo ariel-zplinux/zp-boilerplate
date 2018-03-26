2520be54d0801a8f9fff783010bcc5ad5f33e2eb
Ariel Biton <ariel@zplinux.com>
2018-03-26 15:30:10 +0200

    Add testing (Jest, Enzyme)

---
+++ components/ui/Status.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Message, Icon } from 'semantic-ui-react';
+
+import Status from './Status.js';
+
+describe('<Status />', () => {
+    it('Should contains one Message', () => {
+        const wrapper = shallow(<Status/>);
+        expect(wrapper.find(Message)).toHaveLength(2);
+    })
+});
