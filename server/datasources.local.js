2cebf69be27720605b3cd9afff7f0dd727fcc333
Ariel <ariel@zplinux.com>
2019-01-09 18:07:28 +0100

    Use dynamic datasource with Loopback (Mongo detection)

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,95 +1,26 @@
-(async function(){
+// Initialize dotenv package in order to use .env file
+require('dotenv').config();
 
+const port = process.env.DB_MONGO_PORT || 27017;
+const host = process.env.DB_MONGO_HOST || 'localhost';
+const user = process.env.DB_MONGO_USER || '';
+const password = process.env.DB_MONGO_PASSWORD || '';
+const database = process.env.DB_MONGO_DATABASE || 'messages';
 
-const net = require('net');
+const mongoDatasource = {};
 
-function *generator() {
-  console.log('GENERATOR')
+if (process.env.DB_MONGO_HOST) {
+  mongoDatasource.db = {
+    host,
+    port,
+    url: `mongodb://${host}:${port}/${database}`,
+    database,
+    name: 'db',
+    user,
+    password,
+    connector: "mongodb",
+    useNewUrlParser: true
+  };
 
-  yield new Promise((resolve) => {
-    const socket = new net.Socket();
-    const port = process.env.DB_PORT || 27017;
-    const host = process.env.DB_HOST || 'localhost';
-
-    let mongoDatasource = {};
-
-    // mongoDatasource.db = {
-    //   "host": "localhost",
-    //   "port": 27017,
-    //   "url": "mongodb://localhost:27017/messages",
-    //   "useNewUrlParser":"true",
-    //   "database": "messages",
-    //   "password": "",
-    //   "name": "db",
-    //   "user": "",
-    //   "connector": "mongodb"
-    // };
-
-    // module.exports = mongoDatasource;
-    // console.log({module})
-    console.log({host,port})
-    socket.setTimeout(2000, () => {
-      console.log('TIMEOUT')
-      socket.destroy()
-    });
-
-    socket.connect(port, host, () => {
-      console.log('INSIDE')
-
-      mongoDatasource.db = {
-        "host": "localhost",
-        "port": 27017,
-        "url": "mongodb://localhost:27017/messages",
-        "useNewUrlParser":"true",
-        "database": "messages",
-        "password": "",
-        "name": "mongo",
-        "user": "",
-        "connector": "mongodb"
-      };
-
-      // console.log({module})
-      // process.exit(0)
-      // return cb(mongoDatasource)
-      // return mongoDatasource;
-    });
-
-    socket.on('close', () => {
-      console.log('CLOSE');
-      resolve(mongoDatasource);
-      // if (mongoDatasource.db)  module.exports = mongoDatasource;
-      // console.log({module})
-      // process.exit(0)
-      // return cb(mongoDatasource)
-      // return mongoDatasource;
-    })
-
-  } )
+  module.exports = mongoDatasource;
 }
-
-async function exportF() {
-  const gen = generator();
-  console.log({gen})
-
-  // const exportFunction = (cb) => {
-  //   const gen = generator();
-  //   console.log({gen})
-  //   return gen.next().value;
-
-  // }
-
-  const v = await gen.next().value
-  console.log({v})
-  return v;
-}
-module.exports = exportF();
-
-// socket.on('data', () => {
-//   console.log('DATA')
-// })
-
-// socket.on('error', () => {
-
-//   console.log('ERROR')
-// })
-})()
